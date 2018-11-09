b8fa20b0cd3e832fbd604d81e130abbeace66733
7sena <40254036+7sena@users.noreply.github.com>
2018-11-09 23:59:05 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -3,51 +3,11 @@ const Discord = require('discord.js');
 const client = new Discord.Client();
 var ServerID = "505995244672909323"; 
 var ChannelID = "506014273345159168";
-
-
-    console.log(`Online`);
-client.on('warn', console.warn);
-
-client.on('error', console.error);
-
-
-function timerFunc() {
-    client.on('message', msg => {
-        client.guilds.get(ServerID).channels.get(ChannelID).send(Math.random().toString(36).substring(7)) // Codes server - !.Zeyad' , â™ڑRoأ½ale#5555
-
-
-    });
-}
-
-var timer = setTimeout(timerFunc, 1);
-
-         client.on('message', message => {
-            if (message.content === 'ديلي ') {
-              message.channel.send('#daily');
-              message.channel.sendFile("./photoshop.PNG");
-               
-
-            }
+ console.log(`Online`);
+client.on('ready', async() => {
+    setInterval(()=>{
+    client.guilds.get(ServerID).channels.get(ChannelID).send('**Legends NetWork Large erore king , Legends NetWork Large erore king , Legends NetWork Large erore king , Legends NetWork Large erore king , Legends NetWork Large erore king , Legends NetWork Large erore king , Legends NetWork Large erore king , Legends NetWork Large erore king , Legends NetWork Large erore king , Legends NetWork Large erore king , Legends NetWork Large erore king , Legends NetWork Large erore king , Legends NetWork Large erore king , Legends NetWork Large erore king , Legends NetWork Large erore king , Legends NetWork Large erore king , Legends NetWork Large erore king , Legends NetWork Large erore king , Legends NetWork Large erore king , Legends NetWork Large erore king , Legends NetWork Large erore king , Legends NetWork Large erore king , Legends NetWork Large erore king , Legends NetWork Large erore king , Legends NetWork Large erore king , Legends NetWork Large erore king , Legends NetWork Large erore king , Legends NetWork Large erore king , Legends NetWork Large erore king , Legends NetWork Large erore king , Legends NetWork Large erore king , Legends NetWork Large erore king , Legends NetWork Large erore king , Legends NetWork Large erore king , Legends NetWork Large erore king , Legends NetWork Large erore king , Legends NetWork Large erore king , Legends NetWork Large erore king , Legends NetWork Large erore king , Legends NetWork Large erore king , Legends NetWork Large erore king , Legends NetWork Large erore king , Legends NetWork Large erore king ,Legends NetWork Large erore king , Legends NetWork Large erore king , Legends NetWork Large erore king , Legends NetWork Large erore king , Legends NetWork Large erore king , Legends NetWork Large erore king , Legends NetWork Large erore king , Legends NetWork Large erore king , Legends NetWork Large erore king , Legends NetWork Large erore king , Legends NetWork Large erore king , Legends NetWork Large erore king , Legends NetWork Large erore king , Legends NetWork Large , **')
+    },205);
 });
-
-         client.on('message', message => {
-            if (message.content === '10') {
-              message.channel.send('#credits @™🖤𝓕𝑶𝑿🖤 | Fox Bank 10000');
-              message.channel.sendFile("./photoshop.PNG");
-               
-
-            }
-});
-
-
-         client.on('message', message => {
-            if (message.content === '5') {
-              message.channel.send('#credits @™🖤𝓕𝑶𝑿🖤 | Fox Bank 5000');
-              message.channel.sendFile("./photoshop.PNG");
-               
-
-            }
-});
-
 
 client.login("NTAzNjE1NjMyNTA0Mzg5NjMy.Drb8RQ.D0o10e9c5JPX8NLAPQwfHwszRWc");
